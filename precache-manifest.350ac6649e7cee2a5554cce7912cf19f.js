b04c342c59c4299c09317bf852b0c3063c928c36
JustynaKop <justyna.kopiec@o2.pl>
2019-10-06 22:28:07 +0200

    Updates

---
+++ precache-manifest.350ac6649e7cee2a5554cce7912cf19f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ec0896eb0a781e75897554ae9c8d51",
+    "url": "/CinemaApp/index.html"
+  },
+  {
+    "revision": "5471e89cc89154a52f51",
+    "url": "/CinemaApp/static/css/2.3789fda8.chunk.css"
+  },
+  {
+    "revision": "e618bdc1b6bf548a8b12",
+    "url": "/CinemaApp/static/css/main.1887c979.chunk.css"
+  },
+  {
+    "revision": "5471e89cc89154a52f51",
+    "url": "/CinemaApp/static/js/2.385daeeb.chunk.js"
+  },
+  {
+    "revision": "e618bdc1b6bf548a8b12",
+    "url": "/CinemaApp/static/js/main.0da495a4.chunk.js"
+  },
+  {
+    "revision": "54699bee0ea4a539b836",
+    "url": "/CinemaApp/static/js/runtime-main.2dc9791e.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/CinemaApp/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/CinemaApp/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/CinemaApp/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/CinemaApp/static/media/slick.f97e3bbf.svg"
+  }
+]);
